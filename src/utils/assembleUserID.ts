b46c58b8b1db6d6200e85c527c11c60bdfc8bf6f
Roei <roeioren10@gmail.com>
2021-10-04 16:15:00 +0300

    fix @@ from userID

--- src/utils/assembleUserID.ts
+++ src/utils/assembleUserID.ts
@@ -3,4 +3,4 @@ import { DOMAIN_SUFFIXES } from '../config/enums';
 
 const domainSuffixes: Map<string, string> = new Map<string, string>(DOMAIN_SUFFIXES);
 
-export default (record: matchedRecordType): string => `${record.userID!.split('@')[0]}@${domainSuffixes.get(record.source!)}`.toLowerCase();
+export default (record: matchedRecordType): string => `${record.userID!.split('@')[0]}${domainSuffixes.get(record.source!)}`.toLowerCase();
