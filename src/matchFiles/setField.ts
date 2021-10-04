e965960dd10965ff5b477677d5fbaedcab9378ec
Roei <roeioren10@gmail.com>
2021-10-04 14:33:19 +0300

    add 0 at the start of ohone number

--- src/matchFiles/setField.ts
+++ src/matchFiles/setField.ts
@@ -6,6 +6,13 @@ export default (matchedRecord: matchedRecordType, value: string, fieldName: stri
 };
 
 export const setPhone = (matchedRecord: matchedRecordType, value: string | string[], fieldName: string): void => {
-    if (!Array.isArray(value)) value = [value.replace('-', '')];
-    matchedRecord[fieldName] = value.map((val) => val.replace('-', ''));
+    if (!Array.isArray(value)) value = [value];
+
+    // Add 0 at the start and remove '-' from phone numbers
+    const phoneNumbers: string[] = value.map((num) => {
+        num = num.replace('-', '');
+        return num.startsWith('0') ? num : `0${num}`;
+    });
+
+    matchedRecord[fieldName] = phoneNumbers;
 };
