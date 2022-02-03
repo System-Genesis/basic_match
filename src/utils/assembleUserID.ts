babb850978a2a81f2ebcad84ecc04e8a9a3ee3b4
Roei <roeioren10@gmail.com>
2022-02-03 10:44:17 +0200

    fix documentation

--- src/utils/assembleUserID.ts
+++ src/utils/assembleUserID.ts
@@ -5,7 +5,7 @@ const domainSuffixes: Map<string, string> = new Map<string, string>(DOMAIN_SUFFI
 
 /**
  *  Generating a userID based on the record's source
- * @param { matchedRecordType } - The generated record
+ * @param { matchedRecordType } record - The generated record
  * @return { string } - A userID
  */
 export default (record: matchedRecordType): string => `${record.userID!.split('@')[0]}${domainSuffixes.get(record.source!)}`.toLowerCase();
